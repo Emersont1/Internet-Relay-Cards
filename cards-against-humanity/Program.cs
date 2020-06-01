adcf140473d05868204f2c5fac680e9a2e8e3cc9
Peter Taylor <info@emersont1.co.uk>
2020-06-01 17:23:36 +0100

    Added Project Template Added project template for cards against humanity bot

---
+++ cards-against-humanity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cards_against_humanity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
