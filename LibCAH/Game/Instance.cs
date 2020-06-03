4a6ebf3a80453318cd0315491877909bf99bba4a
Peter Taylor <info@emersont1.co.uk>
2020-06-03 09:37:27 +0100

    working on game stuff first commit on game stuff

---
+++ LibCAH/Game/Instance.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace LibCAH.Game {
+    class Instance {
+
+    }
+}
---
+++ LibCAH/Game/Player.cs
@@ -0,0 +1,10 @@
+using System;
+using LibCAH.Card;
+
+namespace LibCAH.Game {
+    class Player {
+        List<White> Hand;
+        int Score;
+        String Nick;
+    }
+}
